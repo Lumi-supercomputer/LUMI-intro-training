d7285f40fb553d4c18594f219e8ed71dc9b49b28
Kurt Lust <Kurt.Lust@uantwerpen.be>
2024-12-09 09:28:07 +0100

    Improvement to the make_tar.sh script so that it can also work on LUMI.

--- make_tar.sh
+++ make_tar.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
-gtar -vcf exercises.tar --exclude "solution*.sh" --exclude "*.x" --exclude ".ptp*" --exclude "slurm*.out" LICENSE CPE EasyBuild
+# We really want GNU tar which is the tar command on Linux, but, e.g., on BSD-derived 
+# OSes like macOS it needs to be installed separately and the BSD tar does not offer 
+# the options we want.
+if command -v gtar >&/dev/null 
+then TAR=gtar
+else TAR=tar
+fi
+
+$TAR -vcf exercises.tar \
+    --exclude "solution*.sh" --exclude "*.x" --exclude ".ptp*" --exclude "slurm*.out" --exclude  ".DS_Store" \
+    LICENSE CPE EasyBuild
